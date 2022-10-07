ab8b8da679a32aa317934b885fa90868cdd2deba
korovkaK22 <115235686+korovkaK22@users.noreply.github.com>
2022-10-07 16:41:03 +0300

    comm

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+
+
+    }
+}
